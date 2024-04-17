5a84207dda6029557f23989e2e0bab5398df121d
imyoursatan <teguhrinaldi23@gmail.com>
2024-04-17 19:41:26 +0700

    rev 1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 					</p>
 					<div className="">
 						<Link
-							to="portfolio"
+							to="project"
 							smooth
 							duration={500}
 							className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
